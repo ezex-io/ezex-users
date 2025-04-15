7a3a7139aaee5a2506a4c303227702d45cea9609
Danh Nguyen <nguyendanhtravel@gmail.com>
2025-04-15 20:15:28 +0700

    chore: use internal libs load env

--- internal/infra/config/config.go
+++ internal/infra/config/config.go
@@ -2,20 +2,18 @@
 package config
 
 import (
-	"os"
+	"github.com/ezex-io/gopkg/env"
 )
 
 // Config holds all configuration for the application.
 type Config struct {
-	HTTPServerAddress string
 	GRPCServerAddress string
 }
 
 // Load loads configuration from environment variables.
 func Load() (*Config, error) {
 	cfg := &Config{
-		HTTPServerAddress: getEnv("EZEX_USERS_HTTP_SERVER_ADDRESS", ":8080"),
-		GRPCServerAddress: getEnv("EZEX_USERS_GRPC_SERVER_ADDRESS", "0.0.0.0:50051"),
+		GRPCServerAddress: env.GetEnv[string]("EZEX_USERS_GRPC_SERVER_ADDRESS", env.WithDefault("0.0.0.0:50051")),
 	}
 
 	return cfg, nil
@@ -23,22 +21,9 @@ func Load() (*Config, error) {
 
 // BasicCheck performs basic validation of the configuration.
 func (c *Config) BasicCheck() error {
-	if c.HTTPServerAddress == "" {
-		return nil
-	}
-
 	if c.GRPCServerAddress == "" {
 		return nil
 	}
 
 	return nil
-}
-
-func getEnv(key, defaultValue string) string {
-	value := os.Getenv(key)
-	if value == "" {
-		return defaultValue
-	}
-
-	return value
 }
