4c314f4d4a336b8baa7144e3ef27194c05dc1782
Danh Nguyen <nguyendanhtravel@gmail.com>
2025-04-18 08:10:48 +0700

    chore: rm redundant

--- internal/core/port/repository/user_repository.go
+++
@@ -1,8 +0,0 @@
-// Package repository defines the interfaces for repository implementations.
-package repository
-
-// UserRepository defines the interface for all user-related data access.
-type UserRepository interface {
-	// SecurityImage returns the security image repository implementation.
-	SecurityImage() SecurityImageRepository
-}
