e5a05f5f4cf81ce915d9b3c753e600ca9cc6e275
Javad <ja7ad@live.com>
2025-05-13 13:12:59 +0330

    feat: grpc: Add gRPC server configuration
    This commit introduces the gRPC server configuration, including address, health check, and reflection options, loaded from environment variables.

---
+++ internal/adapter/grpc/config.go
@@ -0,0 +1,24 @@
+package grpc
+
+import "github.com/ezex-io/gopkg/env"
+
+type Config struct {
+	Address           string
+	EnableHealthCheck bool
+	// EnableReflection enable reflection option in grpc to load rpc method on client (postman) without proto
+	EnableReflection bool
+}
+
+func LoadFromEnv() *Config {
+	cfg := &Config{
+		Address:           env.GetEnv[string]("EZEX_USERS_GRPC_ADDRESS", env.WithDefault("0.0.0.0:50051")),
+		EnableHealthCheck: env.GetEnv[bool]("EZEX_USERS_GRPC_ENABLE_HEALTHCHECK", env.WithDefault("false")),
+		EnableReflection:  env.GetEnv[bool]("EZEX_USERS_GRPC_ENABLE_REFLECTION", env.WithDefault("false")),
+	}
+
+	return cfg
+}
+
+func (*Config) BasicCheck() error {
+	return nil
+}
